32729ee2bf0f5f70610ce3d734259adf349ce072
mknizewski <mateusz.knizewski@gmail.com>
2016-10-15 16:04:13 +0200

    #infractrucutre - dodanie fabryki widoków.

---
+++ SeeSharp.BO/Dictionaries/NavigationDictionary.cs
@@ -0,0 +1,8 @@
+﻿namespace SeeSharp.BO.Dictionaries
+{
+    public static class NavigationDictionary
+    {
+        public static string MainView = "Strona Główna";
+        public static string AboutAuthorsView = "Autorzy";
+    }
+}
--- SeeSharp/MainPage.xaml.cs
+++ SeeSharp/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SeeSharp.BO.Dictionaries;
+using SeeSharp.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -14,14 +16,21 @@ namespace SeeSharp
 {
     public partial class MainPage : UserControl
     {
+        private const string SectionPrefix = "Jesteś w sekcji: ";
+
         public MainPage()
         {
             InitializeComponent();
+            this.ProgressCircle.Percentage = 25.0;
+            this.SectionBlock.Text = SectionPrefix + NavigationDictionary.MainView;
         }
 
-        private void CircularProgressBar_Loaded(object sender, RoutedEventArgs e)
+        private void AboutAuthors_Click(object sender, RoutedEventArgs e)
         {
-
+            this.DynamicView.Children.Clear();
+            this.DynamicView.Children.Add(ViewFactory.GetView(ViewType.AboutAuthors));
+            this.DynamicView.UpdateLayout();
+            this.SectionBlock.Text = SectionPrefix + NavigationDictionary.AboutAuthorsView;
         }
     }
 }
