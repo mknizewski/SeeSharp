9be173b877888b31f8cb4559299ce62820266587
mknizewski <mateusz.knizewski@gmail.com>
2016-11-22 17:07:18 +0100

    #modules - pierwsza struktura trzymania modulow

--- SeeSharp.BO/Managers/ModuleManager.cs
+++ SeeSharp.BO/Managers/ModuleManager.cs
@@ -1,19 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SeeSharp.BO.Managers
 {
     public class ModuleManager : IDisposable
     {
-        public string ModuleName { get { return _moduleName; } set { _moduleName = value; } }
-        public string Tag { get { return _tag; } set { _tag = value; } }
+        public string ModuleName { get { return _currentModuleName; } set { _currentModuleName = value; } }
+        public string Tag { get { return _currentTag; } set { _currentTag = value; } }
 
-        private string _moduleName;
-        private string _tag;
+        private static List<Module> _moduleList;
+        private static List<Exam> _examList;
 
+        private string _currentModuleName;
+        private string _currentTag;
+            
         private ModuleManager(string moduleName, string tag)
         {
-            _moduleName = moduleName;
-            _tag = tag;
+            _currentModuleName = moduleName;
+            _currentTag = tag;
+
+            InitializeList();
         }
 
         public static ModuleManager GetModuleManager(string moduleName, string tag)
@@ -23,8 +29,8 @@ namespace SeeSharp.BO.Managers
 
         public void Dispose()
         {
-            _moduleName = string.Empty;
-            _tag = string.Empty;
+            _currentModuleName = string.Empty;
+            _currentTag = string.Empty;
         }
 
         public void LoadModule()
@@ -40,5 +46,45 @@ namespace SeeSharp.BO.Managers
         {
             return string.Empty;
         }
+
+        #region List Initializer
+        private void InitializeList()
+        {
+            // Rozdział I
+            _moduleList.Add(Module.CreateModule("Czym jest .NET?", "1.1", false));
+            _moduleList.Add(Module.CreateModule("Dlaczego C#", "1.2", false));
+
+            // Rozdział II
+
+        }
+        #endregion
+    }
+
+    public struct Module
+    {
+        public string ModuleName { get; set; }
+        public string ModuleTag { get; set; }
+        public bool IsExamNext { get; set; }
+
+        public Module(string moduleName, string moduleTag, bool isExamNext) : this()
+        {
+            this.ModuleName = moduleName;
+            this.ModuleTag = moduleTag;
+            this.IsExamNext = isExamNext;
+        }
+
+        public static Module CreateModule(string moduleName, string moduleTag, bool isExamNext)
+        {
+            return new Module(moduleName, moduleTag, isExamNext);
+        }
+    }
+
+    public struct Exam
+    {
+        public string ModuleTag { get; set; }
+        public string Task { get; set; }
+        public string Code { get; set; }
+        public string TestOutput { get; set; }
+        public string TestInput { get; set; }
     }
 }
